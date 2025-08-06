8e374d8d54ac1a362687f51d1326ba108a4a0e73
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-06 15:44:56 -0600

    dashboard useroutes

--- src/routes/userRoutes/dashboard.routes.ts
+++ src/routes/userRoutes/dashboard.routes.ts
@@ -5,3 +5,5 @@ import { authUser } from "@/middlewares/authUser";
 import { getDashboardAlumno } from "@/controllers/userControllers/dashboard.controller";
 
 router.get("/", authUser, getDashboardAlumno);
+
+export default router;
