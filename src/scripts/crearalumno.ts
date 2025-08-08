db2c92dceb32e66fa70af2cebb50d008bc8529ef
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-07 22:33:49 -0600

    night mode, crearalumno script

--- src/scripts/crearalumno.ts
+++ src/scripts/crearalumno.ts
@@ -1,28 +1,23 @@
-import { prisma } from "@/lib/prisma"
-import { hash } from "argon2"
+import { prisma } from "@/lib/prisma";
+import { hash } from "argon2";
 
 async function main() {
-    const passwordHash = await hash("Juanpa13")
+  const passwordHash = await hash("Juanpa13");
 
-    const alumno = await prisma.alumno.create({
-        data: {
-            nombre: "Juan Pablo",
-            correo: "al280622@edu.uaa.mx",
-            matricula: 280622,
-            semestre: 5,
-            passwordHash,
-        },
-    })
-
-    console.log("Alumno creado:", alumno)
+  const admin = await prisma.administrador.create({
+    data: {
+      nombre: "Juan Pablo",
+      correo: "al280622@edu.uaa.mx",
+      passwordHash,
+    },
+  });
 }
 
 main()
-    .catch((e) => {
-        console.error("Error al crear alumno:", e)
-        process.exit(1)
-    })
-    .finally(() => {
-        prisma.$disconnect()
-    })
-
+  .catch((e) => {
+    console.error("Error al crear alumno:", e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
